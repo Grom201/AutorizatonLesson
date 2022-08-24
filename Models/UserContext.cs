d9143d023bebfbad9e971198138e22f7345aaae9
Grom201 <Grom201@DESKTOP-SB66BH6>
2022-08-24 12:37:32 +0300

    Добавьте файлы проекта.

---
+++ Models/UserContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AutorizatonLesson.Models
+{
+    public class UserContext:DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public UserContext(DbContextOptions<UserContext> options)
+            : base(options)
+        {
+           // Database.EnsureDeleted();
+            Database.EnsureCreated();
+        }
+    }
+}
